bbbdcca2cbe821124c8e157b1759e2697c0ffde3
Oleh Yatsenko <mjp.kiev@gmail.com>
2025-11-01 23:40:02 +0200

    Update NoteList.tsx

--- components/NoteList/NoteList.tsx
+++ components/NoteList/NoteList.tsx
@@ -29,14 +29,21 @@ export default function NoteList({ notes }: NoteListProps) {
       {notes.map((note) => (
         <li key={note.id} className={css.listItem}>
           <h3 className={css.title}>{note.title}</h3>
-
           <p className={css.content}>{note.content}</p>
 
           <div className={css.footer}>
-            <span className={css.tag}>{note.tag}</span>
+          
+            {note.tags && note.tags.length > 0 && (
+              <ul className={css.tagsList}>
+                {note.tags.map((tag) => (
+                  <li key={tag} className={css.tag}>
+                    {tag}
+                  </li>
+                ))}
+              </ul>
+            )}
 
             <div>
-              
               <Link
                 href={`/notes/${note.id}`}
                 prefetch={false}
